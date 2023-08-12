eed2b87e279ab409418e0aff09b7aac6f6920535
ffsgfy <somefortera@gmail.com>
2023-08-12 12:51:08 +0500

    extract training and prediction into common generic functions

--- lstm.py
+++ lstm.py
@@ -1,115 +1,78 @@
-import time
-import math
-
-import numpy as np
+import fire
 import torch as th
-from tqdm import tqdm
 
-import dataset
+import utils
 
 
-class Model(th.nn.Module):
+class Model(utils.ModelBase):
     def __init__(self):
         super().__init__()
-        self.encoder = th.nn.Embedding(dataset.EventFormat.EVENT_COUNT, 64)
-        self.lstm = th.nn.LSTM(input_size=64, hidden_size=384, num_layers=2, dropout=0.5)
+
+        d_input = 64
+        d_model = 512
+        self.encoder = th.nn.Embedding(self.data_fmt.EVENT_COUNT, d_input)
+        self.lstm = th.nn.LSTM(
+            input_size=d_input, hidden_size=d_model, num_layers=2, dropout=0.5, batch_first=True
+        )
         self.decoder = th.nn.Sequential(
             th.nn.Dropout(0.5),
-            th.nn.Linear(in_features=384, out_features=512),
-            th.nn.LayerNorm(512),
+            th.nn.Linear(in_features=d_model, out_features=d_model * 2),
+            th.nn.LayerNorm(d_model * 2),
             th.nn.ReLU(),
             th.nn.Dropout(0.5),
-            th.nn.Linear(in_features=512, out_features=256),
-            th.nn.LayerNorm(256),
+            th.nn.Linear(in_features=d_model * 2, out_features=d_model),
+            th.nn.LayerNorm(d_model),
             th.nn.ReLU(),
             th.nn.Dropout(0.5),
-            th.nn.Linear(in_features=256, out_features=dataset.EventFormat.EVENT_COUNT),
+            th.nn.Linear(in_features=d_model, out_features=self.data_fmt.EVENT_COUNT),
         )
-
-        self.data_fmt = dataset.EventFormat()
 
     def forward(
         self, x: th.Tensor, hx: tuple[th.Tensor, th.Tensor] | None = None, sequence: bool = False
     ) -> th.Tensor:
         x = self.lstm(self.encoder(x), hx)[0]
-        x = self.decoder(x if sequence else x[-1])
-        return th.nn.functional.log_softmax(x, dim=-1)
+        return self.decoder(x if sequence else x[..., -1, :])
 
-    def predict(
-        self, data: np.ndarray, length: int, temperature: float = 1.0, masked: bool = False
-    ) -> np.ndarray:
-        device = next(self.parameters()).device
-        x = th.LongTensor(data).to(device)
-        hx = None
-        mask = th.ones(self.data_fmt.EVENT_COUNT, dtype=bool).to(device)  # next token mask
-        result = []
 
-        with th.no_grad():
-            for i in range(length):
-                if masked:
-                    mask = th.BoolTensor(self.data_fmt.next_mask(x[-1])).to(device)
+def train(
+    model_id: str,
+    state_path: str | None = None,
+    epoch_size: int | float | tuple[int | float, int | float] = 1.0,
+    epoch_start: int | None = None,
+    epoch_limit: int = 50,
+    window: int = 256,
+    batch_size: int | tuple[int, int] = 64,
+    lr: float = 0.01,
+    lr_decay: float = 0.9,
+    lr_limit: float = 0.0,
+    sequence: bool = False
+):
+    def batch_fn(batch: th.Tensor) -> tuple[th.Tensor, th.Tensor]:
+        nonlocal sequence
 
-                x, hx = self.lstm(self.encoder(x), hx)
-                x = self.decoder(x[-1])
-                x[~mask] = -th.inf
+        inputs = batch[:, :-1]
+        targets = batch[:, 1:]
+        if not sequence:
+            targets = targets[:, -1]
 
-                if math.isclose(temperature, 0.0, abs_tol=1e-9):
-                    x = th.argmax(x, dim=-1, keepdim=True)
-                else:
-                    x = th.nn.functional.softmax(x / temperature, dim=-1)
-                    x = th.multinomial(x, 1)
+        return (inputs, targets)
 
-                result.append(int(x))
+    def loss_fn(model: Model, batch: tuple[th.Tensor, th.Tensor]) -> th.Tensor:
+        nonlocal sequence
 
-        return np.array(result)
+        inputs, targets = batch
+        outputs = th.nn.functional.log_softmax(model(inputs, sequence=sequence), dim=-1)
+        if sequence:
+            outputs = outputs.transpose(-1, -2)
+
+        return th.nn.functional.nll_loss(outputs, targets)
+
+    utils.train(
+        Model(), model_id, batch_fn, loss_fn,
+        state_path, epoch_size, epoch_start, epoch_limit, window + 1, batch_size, lr, lr_decay, lr_limit
+    )
 
 
 if __name__ == "__main__":
-    th.manual_seed(42)
-    device = th.device("cuda" if th.cuda.is_available() else "cpu")
-    window = 256
-    batch_size = 64
-
-    model_ts = int(time.time())
-    model = Model().to(device)
-    loss_fn = th.nn.NLLLoss()
-    optimizer = th.optim.Adamax(model.parameters(), lr=0.01)
-
-    train_ds = dataset.load_folder("input/ff/train/", window=window, fmt=model.data_fmt)
-    train_dl = th.utils.data.DataLoader(train_ds, batch_size=batch_size, shuffle=True)
-    test_ds = dataset.load_folder("input/ff/test/", window=window, fmt=model.data_fmt)
-    test_dl = th.utils.data.DataLoader(test_ds, batch_size=batch_size, shuffle=False)
-
-    for epoch in range(30):
-        model_path = f"models/lstm-{model_ts}-{epoch}.pth"
-        print("\n", model_path)
-
-        model.train()
-        loss_sum = 0.0
-        for i, (inputs, targets) in enumerate(tqdm_it := tqdm(train_dl, desc="Training")):
-            inputs = inputs.transpose(0, 1).to(device)
-            targets = targets.to(device)
-
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = loss_fn(outputs, targets)
-            loss.backward()
-            optimizer.step()
-
-            loss_sum += float(loss)
-            tqdm_it.set_description(f"Training ({loss_sum / (i + 1):.05f})")
-
-        th.save(model.state_dict(), model_path)
-
-        model.eval()
-        loss_sum = 0.0
-        with th.no_grad():
-            for i, (inputs, targets) in enumerate(tqdm_it := tqdm(test_dl, desc="Testing")):
-                inputs = inputs.transpose(0, 1).to(device)
-                targets = targets.to(device)
-                outputs = model(inputs)
-                loss = loss_fn(outputs, targets)
-                loss_sum += float(loss)
-                tqdm_it.set_description(f"Testing ({loss_sum / (i + 1):.05f})")
-
+    fire.Fire(train)
 
---
+++ utils.py
@@ -0,0 +1,183 @@
+import math
+from typing import Callable, TypeVar
+
+import torch as th
+import numpy as np
+from tqdm import tqdm
+
+import dataset
+
+
+class ModelBase(th.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.data_fmt = dataset.EventFormat()
+
+
+ModelT = TypeVar("ModelT", bound=ModelBase)
+BatchT = TypeVar("BatchT")
+
+
+def train(
+    model: ModelT,
+    # Model identifier used in state filenames
+    model_id: str,
+    # Processes a batch of inputs
+    batch_fn: Callable[[th.Tensor], BatchT],
+    # Calculates and returns the loss
+    loss_fn: Callable[[ModelT, BatchT], th.Tensor],
+    # Optional path to state from which to continue training
+    state_path: str | None,
+    # Tuple = separate train/test epoch sizes; float = fraction of dataset
+    epoch_size: int | float | tuple[int | float, int | float],
+    # Optional first epoch number override
+    epoch_start: int | None,
+    # Maximum number of epochs
+    epoch_limit: int,
+    # Dataset window size
+    window: int,
+    # Tuple = separate train/test batch sizes
+    batch_size: int | tuple[int, int],
+    # Initial learning rate (overrides state)
+    lr: float,
+    # Learning rate exponential decay factor
+    lr_decay: float,
+    # Minimum learning rate
+    lr_limit: float
+):
+    device = th.device("cuda" if th.cuda.is_available() else "cpu")
+    model = model.to(device)
+    optimizer = th.optim.Adamax(model.parameters(), lr=lr)
+
+    if state_path is not None:
+        state = th.load(state_path)
+        model.load_state_dict(state["model"])
+        optimizer.load_state_dict(state["optimizer"])
+        if epoch_start is None:
+            epoch_start = state["last_epoch"] + 1
+
+    # NOTE: initial_lr saved in state is overridden
+    for group in optimizer.param_groups:
+        group["initial_lr"] = lr
+
+    if epoch_start is None:
+        epoch_start = 0
+
+    if isinstance(batch_size, (tuple, list)):
+        batch_size_train, batch_size_test = batch_size
+    else:
+        batch_size_train, batch_size_test = batch_size, batch_size
+
+    scheduler = th.optim.lr_scheduler.LambdaLR(
+        optimizer, lambda epoch: max(lr_decay ** epoch, lr_limit), epoch_start - 1
+    )
+
+    train_ds = dataset.load_folder("input/ff/train/", window=window, fmt=model.data_fmt)
+    train_dl = th.utils.data.DataLoader(train_ds, batch_size=batch_size_train, shuffle=True)
+    test_ds = dataset.load_folder("input/ff/test/", window=window, fmt=model.data_fmt)
+    test_dl = th.utils.data.DataLoader(test_ds, batch_size=batch_size_test, shuffle=True)
+
+    train_total = (len(train_ds) + batch_size_train - 1) // batch_size_train
+    test_total = (len(test_ds) + batch_size_test - 1) // batch_size_test
+
+    if isinstance(epoch_size, (tuple, list)):
+        epoch_size_train, epoch_size_test = epoch_size
+    else:
+        epoch_size_train, epoch_size_test = epoch_size, epoch_size
+
+    if isinstance(epoch_size_train, float):
+        epoch_size_train = int(train_total * epoch_size_train)
+
+    if isinstance(epoch_size_test, float):
+        epoch_size_test = int(test_total * epoch_size_test)
+
+    epoch_size_train = min(epoch_size_train, train_total)
+    epoch_size_test = min(epoch_size_test, test_total)
+
+    for epoch in range(epoch_start, epoch_limit):
+        print(f"\nModel {model_id}, epoch {epoch}")
+
+        model.train()
+        loss_sum = 0.0
+        for i, batch in enumerate(tqdm_it := tqdm(train_dl, "Training", epoch_size_train, dynamic_ncols=True)):
+            if i >= epoch_size_train:
+                break
+
+            batch = batch_fn(batch.to(device))
+            optimizer.zero_grad()
+            loss = loss_fn(model, batch)
+            loss.backward()
+            optimizer.step()
+            loss_sum += float(loss)
+            tqdm_it.set_description(f"Training ({loss_sum / (i + 1):.05f})")
+
+        # This is to allow testing of models without training
+        if epoch_size_train > 0:
+            scheduler.step()
+            state = {
+                "model": model.state_dict(),
+                "optimizer": optimizer.state_dict(),
+                "last_epoch": epoch
+            }
+            state_path = f"models/{model_id}.{epoch}.pth"
+            th.save(state, state_path)
+            print(f"State saved to {state_path}")
+
+        model.eval()
+        loss_sum = 0.0
+        with th.no_grad():
+            for i, batch in enumerate(tqdm_it := tqdm(test_dl, "Testing", epoch_size_test, dynamic_ncols=True)):
+                if i >= epoch_size_test:
+                    break
+
+                batch = batch_fn(batch.to(device))
+                loss = loss_fn(model, batch)
+                loss_sum += float(loss)
+                tqdm_it.set_description(f"Testing ({loss_sum / (i + 1):.05f})")
+
+
+@th.inference_mode()
+def predict(
+    model: ModelBase,  # should return single token
+    window: int,  # model input window length
+    count: int,  # output length
+    temperature: float = 1.0,
+    seed: np.ndarray | None = None,
+    cleanup_model: ModelBase | None = None,  # should return token sequence
+    cleanup_window: int = 1,  # cleanup input window length
+    cleanup_runup: int = 1,  # number of steps to generate before running cleanup
+    cleanup_all: bool = False  # whether cleanup can modify its entire input window (vs only the runup)
+) -> np.ndarray:
+    if seed is None:
+        seed = np.array([0])
+
+    device = next(model.parameters()).device
+    data = th.LongTensor(seed).to(device)
+    mask = th.ones(model.data_fmt.EVENT_COUNT, dtype=bool).to(device)  # next token mask
+    runup = 0
+
+    # WARN: very slow code with exactly zero caching and optimization
+    for i in range(count):
+        if len(data) > 0:
+            mask = th.BoolTensor(model.data_fmt.next_mask(data[-1])).to(device)
+
+        logits = model(data[-window:])
+        logits[~mask] = -th.inf
+
+        if math.isclose(temperature, 0.0, abs_tol=1e-3):
+            result = th.argmax(logits, keepdim=True)
+        else:
+            result = th.multinomial(th.nn.functional.softmax(logits / temperature, dim=-1), 1)
+
+        data = th.cat((data, result))
+
+        if cleanup_model is not None:
+            runup += 1
+            if (len(data) >= cleanup_window) and ((runup >= cleanup_runup) or (i + 1 >= count)):
+                suffix = cleanup_window if cleanup_all else runup
+                data[-suffix:] = th.argmax(cleanup_model(data[-cleanup_window:])[-suffix:], dim=-1)
+                runup = 0
+
+    return np.array(data.cpu())
+
